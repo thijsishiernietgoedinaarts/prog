e42f81eb9aaa39face5513e1bb6c6035c711ecce
thijsishiernietgoedinaarts <43245456+thijsishiernietgoedinaarts@users.noreply.github.com>
2018-10-23 13:16:41 +0200

    Add files via upload

--- project proga/ns2.py
+++ project proga/ns2.py
@@ -1,6 +1,6 @@
 import requests
 import xmltodict
-def getstation():
+def getstation(): #deze functie bepaalt welk station gepakt moet worden
     while True:
         z = []
         y = ''
@@ -18,26 +18,25 @@ def getstation():
                 if y =='exit':
                     break
                 try:
-                    getinformation(y)
+                    getinformation(y) #als de url niet klopt geeft hij een foutmelding
                     return y
                 except: print('dit station staat er niet tussen\nje kan ook afkortingen gebruiken')
         else:
             return 'goed'
-def getinformation(x): #dit haalt alle informatie
+def getinformation(x): #dit haalt alle informatie die nodig is voor de andere functies
     auth_details = ('thijsaarts.aarts@student.hu.nl', '1IbHrkNUhwW6Bnezbn0C9F9_0GKMLSBkXo7vmFW97EHmfaMnVd2Oaw')
     api_url1 = 'http://webservices.ns.nl/ns-api-avt?station='
     api_url2='http://webservices.ns.nl/ns-api-avt?station=ut'
     api_urla = api_url1 + x
     response = requests.get(api_urla, auth=auth_details)
-    with open('vertrektijden.xml', 'w') as myXMLFile:
-        myXMLFile.write(response.text)
+
     vertrekXML = xmltodict.parse(response.text)
+    #dit gedeelte hierboven is alleen voor de informatie halen
 
 
-    #sortedvetreklijst(vertrekXML)
     lijst=[]
     grotelijst=[]
-
+        #deze code is om de xml informatie te veranderen in een dict en daarna in een list
     for vertrek in vertrekXML['ActueleVertrekTijden']['VertrekkendeTrein']:
 
         eindbestemming = vertrek['EindBestemming']
@@ -61,46 +60,25 @@ def getinformation(x): #dit haalt alle informatie
         lijst=[vertrektijd,verbeterdspoort,typetrein,eindbestemming]
         #print('Om '+vertrektijd+' vertrekt een' ,typetrein,'op spoor',spoor['#text'], 'naar '+ eindbestemming)
         grotelijst =grotelijst +[lijst]
-    return grotelijst
+    return grotelijst  #ik gebruik een list omdat die beter zijn te sorteren
 
-def Sortbijspoor(sub_li):
-
-        l = len(sub_li)
-        for i in range(0, l):
-            for j in range(0, l - i - 1):
-                if (sub_li[j][1] > sub_li[j + 1][1]):
-                    tempo = sub_li[j]
-                    sub_li[j] = sub_li[j + 1]
-                    sub_li[j + 1] = tempo
-
-        return sub_li
-
-
-# Driver Code
-def gesortopspoor(x):
-    newx=Sortbijspoor(x)
-    for inf in newx:
-        print('Om',inf[0],'vertrekt een',str(inf[2]),'op spoor',inf[1], 'naar ', str(inf[3]))
-
-def verwijderletter(data, chars): #deze functie probeert de amsterdam spoor te soorteren want die heeft letters bij de nummers
-    new_data = data
-    for ch in chars:
-        new_data = new_data.replace(ch, '')
-    return new_data
 
 def gesortopspooralt(sub_li): #deze functie probeert het spoor te soorteren als er letters staan bij de nummers
+    #ik heb hexadecimaal moeten gebruiken zodat ik sporen zoals 2a of 3 goed kan soorteren
     try: # dit is voor wanneer er een error is met de treinsporen
         l = len(sub_li)
         for i in range(0, l):
             for j in range(0, l - i - 1):
-                try:
-                    nummer=verwijderletter(sub_li[j][1],'ab')
-                except:
-                    nummer=sub_li[j][1]
-                try:
-                    nummer2=verwijderletter(sub_li[j + 1][1],'ab')
-                except:
-                    nummer2=sub_li[j + 1][1]
+                if type(sub_li[j][1]) == int:
+                    numb = str(sub_li[j][1]) + '9'
+                    nummer = int(str(numb), 16)
+                else:
+                    nummer = int(sub_li[j][1], 16)
+                if type(sub_li[j + 1][1]) == int:
+                    numb2 = str(sub_li[j + 1][1]) + '9'
+                    nummer2 = int(str(numb2), 16)
+                else:
+                    nummer2 = int(sub_li[j + 1][1], 16)
                 if (int(nummer) > int(nummer2)):
                     tempo = sub_li[j]
                     sub_li[j] = sub_li[j + 1]
@@ -110,13 +88,14 @@ def gesortopspooralt(sub_li): #deze functie probeert het spoor te soorteren als 
     except ValueError:
             return sub_li
 
-def spooralt(x): #deze functie probeert de amsterdam spoor te soorteren want die heeft letters bij de nummers
+def spooralt(x): #deze functie probeert geeft de spooren weer gesorteerd op spoor
     newx=gesortopspooralt(x)
+    #gesortopspooralt2(newx)
     for inf in newx:
         print('Om',inf[0],'vertrekt een',str(inf[2]),'op spoor',inf[1], 'naar ', str(inf[3]))
 
 
-def Sortbijtijd(sub_li):
+def Sortbijtijd(sub_li): #deze functie sorteerd treinvertrekken op tijd wanneer ze vertrekken
     l = len(sub_li)
     for i in range(0, l):
         for j in range(0, l - i - 1):
@@ -126,25 +105,39 @@ def Sortbijtijd(sub_li):
                 sub_li[j + 1] = tempo
     return sub_li
 print('\n')
-def gesortoptijd(x):
+def gesortoptijd(x):    #deze functie geeft weer wanneer de treinen vertrekken
     newx=Sortbijtijd(x)
     for inf in newx:
         print('Om',inf[0],'vertrekt een',str(inf[2]),'op spoor',inf[1], 'naar ', str(inf[3]))
 
-def spespoor(x):
+def spespoor(x):        #deze functie geeft alles weer van een spoor
     inps= input('van welk spoor wilt u informatie')
     for inf in x:
         if inf[1] == int(inps):
             print('Om', inf[0], 'vertrekt een', str(inf[2]), 'op spoor', inf[1], 'naar ', str(inf[3]))
 
-lijst2=getinformation('ut')
+lijst2=getinformation('db')
 #gesortoptijd(grotelijst2)
 #gesortopspoor(grotelijst2)
 stat='alfabet'
-def route():
+def route():            #deze functie geeft alle treinen weer die van een station naar een eindstation gaan.
     begin= input('waar kom je vandaan')
-    einde= input('waar wil je naartoe')
+    myset={''}
+    lijststat=[]
+    lijststat2=[]
+    lijststat3=[]
     adres=getinformation(begin)
+    for inf in adres:
+        myset.add(inf[3])   #ik heb een set toegevoegd zodat het duidelijker was naar welke stations je kon gaan
+    print('waar wil je naartoe je kan hieruit kiezen')
+    myset.remove('')
+    for i in myset:
+        lijststat.append(i)
+    print(lijststat)
+    lijststat.sort()
+    for m in lijststat:
+        print(m)
+    einde = input('waar wil je naartoe')
     for inf in adres:
         besteming= inf[3].lower()
         if besteming == einde.lower():
@@ -165,11 +158,10 @@ while stat=='goed' or stat=='alfabet':
 
         if a =='spoor':
             lijst2 = getinformation(stat)
-            if errorvertrekspoor == 'true':
+            if errorvertrekspoor == 'true': #dit is voor alls er een probleem zijn met de sporen
                 print('er is een probleem met de sporen.\nalle sporen die onbekend zijn heten nu spoor 0')
-            try:
-                gesortopspoor(lijst2)
-            except: spooralt(lijst2)
+
+            spooralt(lijst2)
             print('\n')
         if a == 'station':
             stat='goed'
@@ -180,3 +172,7 @@ while stat=='goed' or stat=='alfabet':
         if a == 'plan':
             route()
             print('\n')
+
+
+
+
